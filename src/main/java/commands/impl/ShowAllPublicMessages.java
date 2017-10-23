8544172c1761a50dcd6c6626571f05699770f314
NoriX <versatility.95@mail.ru>
2017-10-24 01:54:43 +0300

    Finished: LoadAllData, Load/Save Friendlist

--- src/main/java/commands/impl/ShowAllPublicMessages.java
+++ src/main/java/commands/impl/ShowAllPublicMessages.java
@@ -18,7 +18,8 @@ public class ShowAllPublicMessages implements Command {
         List<Message> ListOfMessages = network.getPublicMessageList();
         System.out.println("Public messages: ");
         for(Message i : ListOfMessages){
-            System.out.println(i.getOwner().getName() + " " + i.getOwner().getSurname()+": "+i.getMessage());
+            System.out.println(i.getOwner().getName() + " " + i.getOwner().getSurname()+" (id = "
+            +i.getOwner().getId()+"): "+i.getMessage());
         }
         try{Thread.sleep(2000);}catch (InterruptedException e){}
     }
